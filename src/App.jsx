047503e6e3043c1e3f7945570d97143879c4caf9
andresfcombac <andresfcc18897@hotmail.com>
2024-07-17 13:44:35 -0500

    conexión inicio de sesion con las rutas del backend desde front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,72 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Busqueda  from './Busqueda'
 
 function App() {
-  const [usuario, setUsuario] = useState('')
-  const [clave, setClave] = useState('')
-  const [logueado, setLogueado] = useState(false)
+  const [usuario, setUsuario] = useState('');
+  const [clave, setClave] = useState('');
+  const [logueado, setLogueado] = useState(false);
 
-  function  cambiarUsuario(evento) {
-    setUsuario(evento.target.value)
+  function cambiarUsuario(evento) {
+    setUsuario(evento.target.value);
   }
 
   function cambiarClave(evento) {
-    setClave(evento.target.value)
+    setClave(evento.target.value);
   }
 
-  function ingresar() {
-    if (usuario == 'admin' && clave == 'admin') {
-      alert('sesion iniciada')  
-      setLogueado(true)
-          } else {
-      alert('usuario o clave incorrectos')
-    }
+  async function ingresar() {
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave,{credentials: 'include'})
+    if (peticion.ok) {
+        setLogueado(true);
+      } else {
+        alert('Usuario o clave inválidas');
+      }
+    
   }
-  
+
+  async function validar (){
+    const peticion = await fetch('http://localhost:3000/validar',{credentials: 'include'})
+    if (peticion.ok) {
+        setLogueado(true);
+      } 
+  }
+  useEffect(()=>{
+     validar()
+  },[])
+
+
   if (logueado) {
-    return <Busqueda/>
+    return <Busqueda />;
   }
-  
+
   return (
     <>
-  <h2>Inicio de sesión</h2>
-  <img src="imagenes/logobc.jpg" alt="Logo BuscaCole" class="logo" ></img>
-  <input placeholder ='usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario}/>    
-  <input placeholder='clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave}/>
-  <button onClick={ingresar}>ingresar</button>
-  <br/>
-  <button>registar </button>
-      </>
-  )
+      <h2>Inicio de sesión</h2>
+      <img src="imagenes/logobc.jpg" alt="Logo BuscaCole" className="logo" />
+      <input
+        placeholder="usuario"
+        type="text"
+        name="usuario"
+        id="usuario"
+        value={usuario}
+        onChange={cambiarUsuario}
+      />
+      <input
+        placeholder="clave"
+        type="password"
+        name="clave"
+        id="clave"
+        value={clave}
+        onChange={cambiarClave}
+      />
+      <button onClick={ingresar}>Ingresar</button>
+      <br />
+      <button>Registrar</button>
+    </>
+  );
 }
 
-export default App
+export default App;
